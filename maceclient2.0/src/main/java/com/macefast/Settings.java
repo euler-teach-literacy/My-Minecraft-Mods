9211eb6135f1f1923f3b910d4d738f9192e37d79
Euler <brian12not13@gmail.com>
2026-04-16 18:06:26 -0400

    Delete maceclient2.0 directory

--- maceclient2.0/src/main/java/com/macefast/Settings.java
+++
@@ -1,8 +0,0 @@
-package com.macefast;
-
-public class Settings {
-    public static boolean maceFastEnabled = true;
-    public static int axeSlot = 0;
-    public static int maceSlot = 1;
-    public static int delay = 4;
-}
